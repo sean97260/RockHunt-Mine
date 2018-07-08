780b28afca4b749e53c7d3ebd8fc52539455f7a7
Clemence Koh <kohclemence@gmail.com>
2018-07-08 10:58:33 -0400

    small files

--- Assets/Scripts/LevelButton.cs
+++ Assets/Scripts/LevelButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Level1Button : MonoBehaviour {
+public class LevelButton : MonoBehaviour {
 
 	bool activated = true;
 
