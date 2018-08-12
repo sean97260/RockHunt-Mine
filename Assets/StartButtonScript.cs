caf0525e612cfbea1475c043cc322f571c530280
Clemence Koh <kohclemence@gmail.com>
2018-08-12 14:40:35 -0400

    Added start scene

---
+++ Assets/StartButtonScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartButtonScript : MonoBehaviour {
+
+    GameObject startButtonShaded;
+
+	// Use this for initialization
+	void Start () {
+        startButtonShaded = GameObject.Find("StartButtonShaded");
+        startButtonShaded.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        startButtonShaded.SetActive(true);
+        SceneManager.LoadScene("Map");
+    }
+
+}
